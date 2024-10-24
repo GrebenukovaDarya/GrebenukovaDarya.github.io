5d710ccf84fabd32398154f67ed5fa5b7c62a559
s0188223 <loved4248@gmail.com>
2024-10-24 11:28:50 +0300

    Update my.js

--- lab7/my.js
+++ lab7/my.js
@@ -1,17 +1,18 @@
-$(document).ready(function(){
-  $(".slider").slick({
-      dots: true,
-      infinite: true,
-      responsive:[{
-          breakpoint: 900,
-          settings: {
-              dots: true,
-              infinite: true,
-              slidesToScroll: 1,
-              slidesToShow: 1
-              }
-      }],
-      slidesToScroll: 1,
-      slidesToShow: 3
-  });
+/*node browser: true */ /*global $ */
+$(document).ready(function () {
+    $(".slider").slick({
+        dots: true,
+        infinite: true,
+        responsive: [{
+            breakpoint: 900,
+            settings: {
+                dots: true,
+                infinite: true,
+                slidesToScroll: 1,
+                slidesToShow: 1
+            }
+        }],
+        slidesToScroll: 1,
+        slidesToShow: 3
+    });
 });
