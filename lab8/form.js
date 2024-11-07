b02da7d1ae719fc1c40be06104c8182630a550da
s0188223 <loved4248@gmail.com>
2024-11-07 11:54:42 +0300

    Update form.js

--- lab8/form.js
+++ lab8/form.js
@@ -1,11 +1,11 @@
 /*node browser: true */ /*global $ */
 window.addEventListener("DOMContentLoaded", (event) => {
     const openBtn = document.getElementById("open-form");
-    const popup = document.getElementbyId("popup");
-    const overlay = document.getElementbyId("overlay");
+    let bpopup = document.getElementbyId("popup");
+    let boverlay = document.getElementbyId("overlay");
       openBtn.addEventListener("click", () => {
-        popup.style.display = "block";
-        overlay.classList.add("show");
+        bpopup.style.display = "block";
+        boverlay.classList.add("show");
         window.history.pushState("", "", "form.html");
       });
 
