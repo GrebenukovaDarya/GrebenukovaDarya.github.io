0bdd21f41aff5c890be6ad36282d1509df63d137
s0188223 <loved4248@gmail.com>
2024-11-10 13:47:43 +0300

    Update form.js

--- lab8/form.js
+++ lab8/form.js
@@ -10,35 +10,49 @@ window.addEventListener("DOMContentLoaded", function () {
         window.history.pushState("", "", "form.html");
     });
 
-      /*
-// Получаем элементы формы
-const form = document.getElementById("myform");
-// Восстанавливаем значения из LocalStorage при загрузке страницы
-window.onload = function() {
-    const storedFio = localStorage.getItem("field-fio:");
-    const storedEmail = localStorage.getItem("field-email");
-    const storedMessage = localStorage.getItem("field-message");
+    window.onload = function () {
+        const storedFio = localStorage.getItem("field-fio");
+        const storedEmail = localStorage.getItem("field-email");
+        const storedMessage = localStorage.getItem("field-message");
+        const storedNumber = localStorage.getItem("field-number");
+        const storedOrg = localStorage.getItem("field-org");
 
-    if (storedFio) {
-        document.getElementsByName("field-fio").value = storedFio;
-    }
-    if (storedEmail) {
-        document.getElementsByName("field-email").value = storedEmail;
-    }
-    if (storedMessage) {
-        document.getElementsByName("field-message").value = storedMessage;
-    }
-};
-// Сохраняем значения в LocalStorage при каждом вводе
-form.addEventListener("input: ", function(event) {
-    const fields = ["field-fio", "field-email", "field-number",
-                    "field-message"];
-    fields.forEach(function (field) {
-        localStorage.setItemfield;
-        document.getElementById(field).value;
+        if (storedFio) {
+            document.getElementsByName("field-fio")[0].value = storedFio;
+        }
+        if (storedEmail) {
+            document.getElementsByName("field-email")[0].value = storedEmail;
+        }
+        if (storedMessage) {
+            document.getElementsByName("field-message")[0].value =
+            storedMessage;
+        }
+        if (storedNumber) {
+            document.getElementsByName("field-number")[0].value = storedNumber;
+        }
+        if (storedOrg) {
+            document.getElementsByName("field-org")[0].value = storedOrg;
+        }
+    };
+
+    const form = document.getElementById("myform");
+
+    form.addEventListener("input", function () {
+        const fields = [
+            "field-fio",
+            "field-email",
+            "field-number",
+            "field-org",
+            "field-message"
+        ];
+        fields.forEach(function (field) {
+            localStorage.setItem(
+                field,
+                document.getElementsByName(field)[0].value
+            );
+        });
     });
-});
-*/
+
 
 
     window.addEventListener("popstate", function (event) {
@@ -50,37 +64,57 @@ form.addEventListener("input: ", function(event) {
         updateContent(event.state.content);
     });
 
-
-
     $(function () {
         $(".formcarryForm").submit(function (e) {
             e.preventDefault();
 
-            $.ajax({
-                complete: function () {
-                    document.getElementById("myform").reset();
-                },
-                contentType: false,
-                data: new FormData(this),
-                dataType: "json",
-                error: function (jqXHR) {
-                    const errorObject = jqXHR.responseJSON;
+            let email = document.getElementsByName("field-email");
+            let name = document.getElementsByName("field-fio");
+            let number = document.getElementsByName("field-number");
+            const checkbox = document.getElementById("formcheck");
+            let formcheck = true;
+            if (!name[0].value) {
+                formcheck = false;
+            }
+            if (!email[0].value) {
+                formcheck = false;
+            }
+            if (!number[0].value) {
+                formcheck = false;
+            }
+            if (!checkbox.checked) {
+                formcheck = false;
+            }
 
-                    alert("Ошибка: " + errorObject.message);
-                },
-                processData: false,
-                success: function (response) {
-                    if (response.status === "success") {
-                        alert("Форма отправлена!");
+            if (formcheck) {
+                $.ajax({
+                    complete: function () {
                         document.getElementById("myform").reset();
-                    } else {
-                        alert("Ошибка");
-                        document.getElementById("myform").reset();
-                    }
-                },
-                type: "POST",
-                url: "https://formcarry.com/s/5HKia9oLyPf"
-            });
+                    },
+                    contentType: false,
+                    data: new FormData(this),
+                    dataType: "json",
+                    error: function (jqXHR) {
+                        const errorObject = jqXHR.responseJSON;
+
+                        alert("Ошибка: " + errorObject.message);
+                    },
+                    processData: false,
+                    success: function (response) {
+                        if (response.status === "success") {
+                            alert("Форма отправлена!");
+                            document.getElementById("myform").reset();
+                        } else {
+                            alert("Ошибка");
+                            document.getElementById("myform").reset();
+                        }
+                    },
+                    type: "POST",
+                    url: "https://formcarry.com/s/5HKia9oLyPf"
+                });
+            } else {
+                alert("Заполните все поля формы");
+            }
         });
     });
 });
