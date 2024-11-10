c067a8437f620593f54338186b1e20fb71b3f14e
s0188223 <loved4248@gmail.com>
2024-11-10 11:23:41 +0300

    Update form.js

--- lab8/form.js
+++ lab8/form.js
@@ -1,17 +1,18 @@
-/*node browser: true */ /*global $ */
-window.addEventListener("DOMContentLoaded", (event) => {
+/*node browser: true */ /*global $ */ /*global alert */
+/*global updateContent */
+window.addEventListener("DOMContentLoaded", function () {
     const openBtn = document.getElementById("open-form");
-    let bpopup = document.getElementById("popup");
-    let boverlay = document.getElementById("overlay");
-      openBtn.addEventListener("click", () => {
-        bpopup.style.display = "block";
-        boverlay.classList.add("show");
+    let popup = document.getElementById("popup");
+    let overlay = document.getElementById("overlay");
+    openBtn.addEventListener("click", function () {
+        popup.style.display = "block";
+        overlay.classList.add("show");
         window.history.pushState("", "", "form.html");
-      });
+    });
 
+      /*
 // Получаем элементы формы
 const form = document.getElementById("myform");
-
 // Восстанавливаем значения из LocalStorage при загрузке страницы
 window.onload = function() {
     const storedFio = localStorage.getItem("field-fio:");
@@ -28,62 +29,58 @@ window.onload = function() {
         document.getElementsByName("field-message").value = storedMessage;
     }
 };
-
 // Сохраняем значения в LocalStorage при каждом вводе
 form.addEventListener("input: ", function(event) {
     const fields = ["field-fio", "field-email", "field-number",
                     "field-message"];
     fields.forEach(function (field) {
-        localStorage.setItemfield; document.getElementById(field).value
+        localStorage.setItemfield;
+        document.getElementById(field).value;
     });
 });
+*/
 
 
-// Обработка событий навигации для контроля за поведением при переходах
-window.addEventListener("popstate", function(event) {
-  if(popup.style.display === "block")
-  {
-    popup.style.display = "none";
-    overlay.classList.remove("show");
-    window.history.replaceState("", "", "index.html");
-  }
-  updateContent(event.state.content);
-});
-
-
-
-$(function() {
-    $(".formcarryForm").submit(function(e){
-        e.preventDefault();
-
-        $.ajax({
-            complete: function(){
-                document.getElementById("myform").reset();
-            },
-            contentType: false,
-            data: new FormData(this),
-            dataType: "json",
-            error: function(jqXHR, textStatus){
-                const errorObject = jqXHR.responseJSON;
-
-                alert("Ошибка: " +
-                      errorObject.title + ": " +
-                      errorObject.message);
-            },
-            processData: false,
-            success: function(response){
-                if(response.status === "success"){
-                    alert("Форма отправлена!");
-                    document.getElementById("myform").reset();
-                }
-                else{
-                    alert("Ошибка");
-                    document.getElementById("myform").reset();
-                }
-          },
-            type: "POST",
-            url: "https://formcarry.com/s/5HKia9oLyPf"
-      });
+    window.addEventListener("popstate", function (event) {
+        if (popup.style.display === "block") {
+            popup.style.display = "none";
+            overlay.classList.remove("show");
+            window.history.replaceState("", "", "index.html");
+        }
+        updateContent(event.state.content);
     });
-});
+
+
+
+    $(function () {
+        $(".formcarryForm").submit(function (e) {
+            e.preventDefault();
+
+            $.ajax({
+                complete: function () {
+                    document.getElementById("myform").reset();
+                },
+                contentType: false,
+                data: new FormData(this),
+                dataType: "json",
+                error: function (jqXHR) {
+                    const errorObject = jqXHR.responseJSON;
+
+                    alert("Ошибка: " + errorObject.message);
+                },
+                processData: false,
+                success: function (response) {
+                    if (response.status === "success") {
+                        alert("Форма отправлена!");
+                        document.getElementById("myform").reset();
+                    } else {
+                        alert("Ошибка");
+                        document.getElementById("myform").reset();
+                    }
+                },
+                type: "POST",
+                url: "https://formcarry.com/s/5HKia9oLyPf"
+            });
+        });
+    });
 });
